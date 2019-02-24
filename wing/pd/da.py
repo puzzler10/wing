c40adca1eac7e3fe90dfcf7151730a515cf17634
Roth (Contractor) Tom <xtrh1@woolworths.com.au>
2019-02-25 09:57:33 +1100

    added two functions to test

---
+++ wing/pd/da.py
@@ -0,0 +1,5 @@
+def da(df, nrow = 100):
+    """Display all columns in a pandas DataFrame.
+    nrow: the number of rows to show"""
+    with pd.option_context("display.max_rows", nrow, "display.max_columns", 1000): 
+        display(df)
